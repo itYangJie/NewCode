e15b6c881e01c435872e0447c4c3b958224fd7d0
itYangJie <1147185600@qq.com>
2018-11-04 15:49:12 +0800

    2018_11_4

--- src/com/xmu/leetcode/ReConstructQueue.java
+++ src/com/xmu/leetcode/ReConstructQueue.java
@@ -1,5 +1,35 @@
 package com.xmu.leetcode;
 
-public class ReConstructQueue {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
+public class ReConstructQueue {
+	
+	public int[][] reconstructQueue(int[][] people) {
+
+		 if (people == null || people.length == 0) {
+	            return people;
+	        }
+	        Arrays.sort(people, new Comparator<int[]>() {
+	            @Override
+	            public int compare(int[] p1, int[] p2) {
+	                return p1[0] == p2[0] ? p1[1] - p2[1] : p2[0] - p1[0];
+	            }
+	        });
+	        List<int[]> temp = new ArrayList<int[]>();
+	        for (int[] aPeople : people) {
+	            if (people.length == aPeople[1]) {
+	                temp.add(aPeople);
+	            } else {
+	                temp.add(aPeople[1], aPeople);
+	            }
+	        }
+	        int ans[][] = new int[people.length][2];
+	        for (int i = 0; i < temp.size(); i++) {
+	            ans[i] = temp.get(i);
+	        }
+	        return ans;
+	}
 }
